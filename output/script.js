83df99e1e8ae53372ad55c5f02baffc2b421a4fd
ziwa4pers <lperezd12@miumg.edu.gt>
2024-08-09 21:50:44 -0600

    Add files via upload

---
+++ output/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+console.log("Hola soy Js");
+var valor1;
+for (var i; i < 10; i++) {
+  console.log(i);
+}
+var numero1;
+var constante = 23;
+var nombre = 'Linda Perez';
+console.log('Hola' + nombre);
+
+// Declare variable for the horizontal position of the circle
+var x = 25;
+function setup() {
+  // Create the canvas
+  createCanvas(720, 400);
+
+  // Set the color mode to hue-saturation-brightness (HSB)
+  colorMode(HSB);
+
+  // Set the text size
+  textSize(20);
+
+  // No animation to start
+  noLoop();
+}
+function draw() {
+  // Clear the background
+  background(0);
+
+  // Draw a circle, with hue determined by frameCount
+  fill(x / 3, 90, 90);
+  circle(x, height / 2, 50);
+
+  // Increase the x variable by 5
+  x += 5;
+
+  // Reset the circle position after it moves off the right side
+  if (x > width + 25) {
+    x = -25;
+  }
+  describe('circle moving to the right');
+}
+function mousePressed() {
+  // Start/stop the animation loop
+  if (isLooping()) {
+    noLoop();
+  } else {
+    loop();
+  }
+}
+function keyPressed() {
+  // Draw one frame
+  redraw();
+}
